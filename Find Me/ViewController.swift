86a8a94600923ed1f9833b35512f71d64b9dc33b
Jack Douglas <Jack@Jacks-iMac.attlocal.net>
2015-08-29 17:05:33 -0700

    Initial Commit

---
+++ Find Me/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Find Me
+//
+//  Created by Jack Douglas on 8/29/15.
+//  Copyright (c) 2015 NanoJack Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
